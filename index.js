5070de08b6d081200b002e47bdeca5ce2ec3d256
Danny Goodrich <dannyngoodrich@gmail.com>
2019-11-10 06:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var array = ["John Lennon plays guitar"];
+  var array = ["John Lennon plays Guitar"];
 for (var i= 0; i < musicians.length; i++) {
   array.push(musicians[i] + ' plays ' + instruments[i]);
   }
